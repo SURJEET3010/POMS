4f714f4b21715e88fbfe501100d59f1e613fbd3b
Surjeet Kumar <70914713+SURJEET3010@users.noreply.github.com>
2023-01-07 00:45:27 +0530

    Update DML.sql

--- Scripts/DML.sql
+++ Scripts/DML.sql
@@ -16,16 +16,16 @@ INSERT INTO `poms`.`Customer`
 `preferred_login_id`)
 VALUES
 ("Alexa","al9090@hmail.com", 9090787856, "Street 12, green tower, Jordan", 
-    "10-09-1999", "xxxxxxx", "visa card", "al9090@hmail.com"),
+    "10-09-1999", 32, "xxxxxxx", "visa card", "al9090@hmail.com"),
     
     ("Alexander","polo90@hmail.com", 9898990908, "Street 3, twin tower, tordan", 
-    "10-02-1992", "xxxxxxx", "visa card", "polo90@hmail.com"),
+    "10-02-1992", 43, "xxxxxxx", "visa card", "polo90@hmail.com"),
     
     ("Marie","al9090@hmail.com", 98989, "Street 3, twin tower, Jordan", 
-    "10-09-1997", "xxxxxxx", "visa card", "al9090@hmail.com"),
+    "10-09-1997", 12, "xxxxxxx", "visa card", "al9090@hmail.com"),
     
     ("Jacky","loki0@hmail.com", 98989, "Street 2,quad tower, london", 
-    "10-09-1992", "xxxxxxx", "visa card", "loki@hmail.com"),
+    "10-09-1992", 44, "xxxxxxx", "visa card", "loki@hmail.com"),
     
     ("Bob","bob@hmail.com", 98989, "Street 3, twin tower, Jordan", 
     "10-09-19992", "xxxxxxx", "visa card", "bob@hmail.com"),
